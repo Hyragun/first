ec9b83392518652daa9e0ef62c9179a9b2118b3d
Hussein Nadzhafov <husejnnadzafov@gmail.com>
2026-09-06 12:07:12 +0200

    First comit

---
+++ first.py
@@ -0,0 +1,22 @@
+# True / False     bool
+#  5            int
+#  1.5          float
+#  "Hello"      str
+
+name = input("Name> ")
+age = input("Age> ")
+power = input("Power> ")
+
+
+print("\n\n  Welcome \t to LABIRONTE! ")
+
+print("   /\\_/\\ ")
+print("  ( o.o ) ")
+print("   > ^ < ")
+
+
+print("Hello,", name)
+print("Your age:", age)
+print("Your power:", power)
+
+# print(type(age))
